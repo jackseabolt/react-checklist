fb1e06acb922e9dfb861f7bd748462b39f640251
Jack Seabolt <jackseabolt@gmail.com>
2017-10-11 15:51:02 -0700

    components seperated

--- src/form.js
+++ src/form.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import { addItem } from './actions'; 
 
 export class Form extends React.Component {
+    
+    handleSubmit(event){
+        event.preventDefault(); 
+        this.props.dispatch(addItem(this.userInput.value))
+        this.userInput.value = ''; 
+    }
+    
     render(){
         return (
             <div> 
                 <div className="form">
-                    <input className="input" placeholder="Add an item" ref={input => this.userInput = input} />
-                    <button className="button" onClick={e => {
-                        this.props.dispatch(addItem(this.userInput.value)) 
-                        this.userInput.value = ''; 
-                    }
-                    }>Add</button> 
+                    <h2>React Checklist</h2>
+                    <form onSubmit={e => this.handleSubmit(e)}>
+                        <input className="input" placeholder="Add an item" ref={input => this.userInput = input} />
+                        <button className="button" type="submit">Add</button> 
+                    </form>
                 </div>
             </div>
         )
